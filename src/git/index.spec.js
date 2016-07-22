a54effbcb20126cfff5bf021fa5b614c64f0845e
Kmaschta <kevin@marmelab.com>
2016-07-22 18:01:14 +0200

    Add test to git library

---
+++ src/git/index.spec.js
@@ -0,0 +1,49 @@
+import { assert } from 'chai';
+import gitFactory from './index';
+
+describe('Git library', () => {
+    const client = {};
+    let options;
+
+    beforeEach(() => {
+        options = {
+            owner: 'marmelab',
+            repository: 'sedy',
+        };
+    });
+
+    it('should throw an error if repository owner is not specified', () => {
+        delete options.owner;
+        assert.throws(() => gitFactory(client, options));
+    });
+
+    it('should throw an error if repository name is not specified', () => {
+        delete options.repository;
+        assert.throws(() => gitFactory(client, options));
+    });
+
+    it('should return blobs api', () => {
+        const git = gitFactory(client, options);
+        assert.include(Object.keys(git), 'blobs');
+    });
+
+    it('should return trees api', () => {
+        const git = gitFactory(client, options);
+        assert.include(Object.keys(git), 'trees');
+    });
+
+    it('should return commits api', () => {
+        const git = gitFactory(client, options);
+        assert.include(Object.keys(git), 'commits');
+    });
+
+    it('should return repository informations', () => {
+        const git = gitFactory(client, options);
+
+        assert.deepEqual(git.repository, {
+            id: 'marmelab/sedy',
+            owner: 'marmelab',
+            name: 'sedy',
+        });
+    });
+});
