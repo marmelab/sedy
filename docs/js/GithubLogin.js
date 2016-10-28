d338796eea8483f597180f9cd1b8d595dd3c4977
Alexis Janvier <contact@alexisjanvier.net>
2016-10-28 15:52:35 +0200

    Debug spotted

--- docs/js/GithubLogin.js
+++ docs/js/GithubLogin.js
@@ -3,7 +3,6 @@ import RaisedButton from 'material-ui/RaisedButton';
 import React, { PropTypes } from 'react';
 
 const GithubLogin = ({appId, scopes, redirectUri}) => {
-    console.log(redirectUri);
     const href = `https://github.com/login/oauth/authorize?client_id=${appId}&scope=${encodeURIComponent(scopes.join(','))}&redirect_uri=${encodeURIComponent(redirectUri)}`;
     return (
         <RaisedButton
