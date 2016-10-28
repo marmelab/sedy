1f8a9bfd64278193b9ef951d560369c9106b1f4c
Alexis Janvier <contact@alexisjanvier.net>
2016-10-28 14:47:02 +0200

    Remove duplicates in conf files

--- docs/config/production-dist.js
+++ docs/config/production-dist.js
@@ -2,8 +2,4 @@ export default {
     appBaseUrl: 'http://localhost:8080',
     githubAppId: 'xxx',
     githubRedirection: '/setup.html',
-    githubScopes: [
-        'write:repo_hook',
-        'repo',
-    ],
 };
