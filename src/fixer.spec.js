61e2bd9421b34a710696881657076ebbf1667719
ThieryMichel <thiery@marmelab.com>
2016-10-28 16:29:21 +0200

    remove unused fixture

--- src/fixer.spec.js
+++ src/fixer.spec.js
@@ -1,6 +1,6 @@
 import { assert } from 'chai';
 import fixerFactory from './fixer';
-import { diffHunkWithAccents, simpleDiffHunk, simpleFile } from './test/fixtures';
+import { diffHunkWithAccents, diffHunk, diffBlob } from './test/fixtures';
 
 describe('Fixer', () => {
     const logger = console;
@@ -9,25 +9,25 @@ describe('Fixer', () => {
     describe('getLineIndexFromDiff', () => {
         it('should return the correct line', () => {
             const fixer = fixerFactory(null, logger);
-            const index = fixer.getLineIndexFromDiff(simpleDiffHunk, 5);
+            const index = fixer.getLineIndexFromDiff(diffHunk, 5);
 
             assert.equal(index, 2);
 
-            assert.equal(simpleFile.split('\n')[index], simpleDiffHunk.split('\n')[5].slice(1));
+            assert.equal(diffBlob.split('\n')[index], diffHunk.split('\n')[5].slice(1));
         });
         it('should return the correct line even if several line are identical', () => {
             const fixer = fixerFactory();
-            let index = fixer.getLineIndexFromDiff(simpleDiffHunk, 7);
+            let index = fixer.getLineIndexFromDiff(diffHunk, 7);
             assert.equal(index, 4);
-            assert.equal(simpleFile.split('\n')[index], simpleDiffHunk.split('\n')[7].slice(1));
+            assert.equal(diffBlob.split('\n')[index], diffHunk.split('\n')[7].slice(1));
 
-            index = fixer.getLineIndexFromDiff(simpleDiffHunk, 6);
+            index = fixer.getLineIndexFromDiff(diffHunk, 6);
             assert.equal(index, 3);
-            assert.equal(simpleFile.split('\n')[index], simpleDiffHunk.split('\n')[6].slice(1));
+            assert.equal(diffBlob.split('\n')[index], diffHunk.split('\n')[6].slice(1));
         });
         it('should return null if position point on a deleted line', () => {
             const fixer = fixerFactory();
-            const index = fixer.getLineIndexFromDiff(simpleDiffHunk, 3);
+            const index = fixer.getLineIndexFromDiff(diffHunk, 3);
 
             assert.isNull(index);
         });
