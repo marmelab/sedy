33c8a592662aabe65af91b4be02fa057de9db84a
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-10-28 15:07:56 +0200

    Add pagination

---
+++ docs/js/setup/repositories/Pagination.js
@@ -0,0 +1,51 @@
+import React, { Component, PropTypes } from 'react';
+import { blue500 } from 'material-ui/styles/colors';
+
+const styles = {
+    pagination: {
+        display: 'flex',
+    },
+    link: {
+        flex: '1 0 0',
+        color: blue500,
+        padding: '0.5rem 1rem',
+    },
+};
+
+const Pagination = ({ hasNext, onChange, page }) => {
+    const previousStyle = {
+        ...styles.link,
+        display: page <= 1 ? 'none' : 'block',
+    };
+
+    const nextStyle = {
+        ...styles.link,
+        textAlign: 'right',
+        display: hasNext ? 'block' : 'none',
+    };
+
+    return (
+        <div style={styles.pagination}>
+            <a href="#" style={previousStyle} onClick={onChange(page - 1)}>
+                &laquo; Previous
+            </a>
+            <a href="#" style={nextStyle} onClick={onChange(page + 1)}>
+                Next &raquo;
+            </a>
+        </div>
+    );
+}
+
+Pagination.propTypes = {
+    hasNext: PropTypes.bool,
+    onChange: PropTypes.func,
+    page: PropTypes.number,
+};
+
+Pagination.defaultProps = {
+    hasNext: false,
+    onChange: () => {},
+    page: 1,
+};
+
+export default Pagination;
--- docs/js/setup/repositories/Repository.js
+++ docs/js/setup/repositories/Repository.js
@@ -1,10 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { TableRow, TableRowColumn } from 'material-ui/Table';
-import { red500, green500 } from 'material-ui/styles/colors';
+import { red400, green500 } from 'material-ui/styles/colors';
 
-import FlatButton from 'material-ui/FlatButton';
 import AddSedy from 'material-ui/svg-icons/content/add';
-import RemoveSedy from 'material-ui/svg-icons/content/clear';
+import CircularProgress from 'material-ui/CircularProgress';
+import FlatButton from 'material-ui/FlatButton';
+import RemoveSedy from 'material-ui/svg-icons/action/delete';
 
 const styles = {
     name: {
@@ -26,26 +27,64 @@ const styles = {
     },
 };
 
-const Repository = ({ repository }) => (
-    <TableRow className="repository">
-        <TableRowColumn>
-            <div>
-                <h5 style={styles.name}>{repository.full_name}</h5>
-                <p style={styles.description}>{repository.description}</p>
-            </div>
-        </TableRowColumn>
-        <TableRowColumn style={styles.actions}>
+class Repository extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hasSedy: props.sedy_installed,
+            loading: false,
+        };
+    }
+
+    toggleSedy = () => {
+        this.setState({ loading: true });
+
+        setTimeout(() => {
+            this.setState({
+                loading: false,
+                hasSedy: !this.state.hasSedy,
+            });
+        }, 2000);
+    };
+
+    actions() {
+        if (this.state.loading) {
+            return <CircularProgress />;
+        }
+
+        return (
             <FlatButton
+                onClick={this.toggleSedy}
                 style={styles.actionButton}
                 icon={
-                    repository.sedy_installed ?
-                    <RemoveSedy color={red500} /> :
+                    this.state.hasSedy ?
+                    <RemoveSedy color={red400} /> :
                     <AddSedy color={green500} />
                 }
             />
-        </TableRowColumn>
-    </TableRow>
-);
+        );
+    }
+
+    render() {
+        const {
+            repository,
+        } = this.props;
+
+        return (
+            <TableRow className="repository">
+                <TableRowColumn>
+                    <div>
+                        <h5 style={styles.name}>{repository.full_name}</h5>
+                        <p style={styles.description}>{repository.description}</p>
+                    </div>
+                </TableRowColumn>
+                <TableRowColumn style={styles.actions}>
+                    {this.actions()}
+                </TableRowColumn>
+            </TableRow>
+        );
+    }
+};
 
 Repository.propTypes = {
     repository: PropTypes.shape({
