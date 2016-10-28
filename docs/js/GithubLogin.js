dac0bd40fd2689ef5710858cfc25d91a179549db
Alexis Janvier <contact@alexisjanvier.net>
2016-10-28 13:41:05 +0200

    Add props to GithubLogin

--- docs/js/GithubLogin.js
+++ docs/js/GithubLogin.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
+import RaisedButton from 'material-ui/RaisedButton';
+import React, { PropTypes } from 'react';
 
-const GithubLogin = () => (
-    <RaisedButton
-        label="Signin with GitHub"
-        icon={<FontIcon className="github-icon" />}
-        href="https://github.com/login/oauth/authorize?client_id=xxx&scope=repo"
-    />
-);
+const GithubLogin = ({appId, scopes}) => {
+    const href = `https://github.com/login/oauth/authorize?client_id=${appId}&scope=${encodeURIComponent(scopes.join(','))}`;
+    return (
+        <RaisedButton
+            label="Signin with GitHub"
+            icon={<FontIcon className="github-icon" />}
+            href={href}
+        />
+    );
+};
+
+GithubLogin.propTypes = {
+    appId: PropTypes.string.isRequired,
+    scopes: PropTypes.array.isRequired,
+};
 
 export default GithubLogin;
