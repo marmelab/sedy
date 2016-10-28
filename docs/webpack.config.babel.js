745678c74816d5d89a27c036f2004b774f215ad4
Jonathan Petitcolas <petitcolas.jonathan@gmail.com>
2016-10-28 11:05:09 +0200

    Bootstrap React on public site

--- docs/webpack.config.babel.js
+++ docs/webpack.config.babel.js
@@ -5,6 +5,7 @@ export default {
     entry: {
         style: './style.css',
         home: './js/home',
+        setup: './js/setup',
     },
     output: {
         filename: 'build/[name].js',
@@ -13,12 +14,19 @@ export default {
         loaders: [
             { test: /\.css$/, include: `${__dirname}/style.css`, loader: ExtractTextPlugin.extract('css') },
             { test: /\.js$/, include: `${__dirname}/js`, loader: 'babel' },
+            { test: /\.svg$/, include: `${__dirname}/icons`, loader: 'file-loader' },
         ]
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './index.html',
             filename: 'index.html',
+            chunks: ['style', 'home'],
+        }),
+        new HtmlWebpackPlugin({
+            template: './setup.html',
+            filename: 'setup.html',
+            chunks: ['style', 'setup'],
         }),
         new ExtractTextPlugin('style.css'),
     ],
