7c1b3bddbbe80be4bb2c87f3596014ff712315fe
djhi <gildas.garcia@gmail.com>
2016-12-20 14:49:17 +0100

    review

--- src/git/commands/checkout.js
+++ src/git/commands/checkout.js
@@ -6,14 +6,12 @@ export default (references, commits) => function* (ref, force = false) {
     // If no: continue
 
     const commitSha = yield references.get(ref);
-    console.log('checkout', { ref, commitSha, force });
 
     if (!commitSha) {
         throw new CommandError(`Reference ${ref} does not exist`);
     }
 
     const commit = yield commits.get(commitSha);
-    console.log('checkout', { commit });
 
     if (!commit) {
         throw new CommandError(`Commit SHA "${commitSha}" from reference "${ref}" does not exist`);
