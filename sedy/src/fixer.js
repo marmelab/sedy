51d6f7f2eaf45c71728375e59e4c0e58e0cc28d7
Luwangel <adrien.amoros@gmail.com>
2018-10-03 16:52:51 +0200

    Manage the case where fixBlob method returns null

--- sedy/src/fixer.js
+++ sedy/src/fixer.js
@@ -42,7 +42,13 @@ export default (git, commiter, logger, parsedContent) => {
         const line = lines[index];
 
         if (!line) {
-            logger.debug('debug infos', { parsedContent, lines, index, diffHunk, position });
+            logger.debug('debug infos', {
+                parsedContent,
+                lines,
+                index,
+                diffHunk,
+                position,
+            });
             return null;
         }
 
@@ -106,17 +112,18 @@ export default (git, commiter, logger, parsedContent) => {
             }
 
             const fix = yield fixBlob(fixRequest, blob, match);
-            lastCommitSha = yield commiter.prepareFix(fixRequest, fix, lastCommitSha);
 
             if (fix) {
+                lastCommitSha = yield commiter.prepareFix(fixRequest, fix, lastCommitSha);
                 fixes.push(fix);
+            } else {
+                logger.error('Unable to fix blob', JSON.stringify({ fixRequest, blob, match }));
             }
         }
 
         // TODO: Warn user that he didn't understood the comment
         return { fixes, commitSha: lastCommitSha };
     };
-
 
     return { fixBlob, getLineIndexFromDiff, processFixRequest };
 };
