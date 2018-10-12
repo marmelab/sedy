840c47f8d35c73dfcbeccfcdd40185f1685eaa03
Kmaschta <kevin@marmelab.com>
2018-10-12 16:40:15 +0200

    Avoid to read GitHub private key on development

--- sedy/githubAppPrivateKey.js
+++ sedy/githubAppPrivateKey.js
@@ -2,6 +2,12 @@ import fs from 'fs';
 
 const env = process.env.NODE_ENV || 'development';
 
-const buffer = fs.readFileSync(`${__dirname}/config/privateKeys/${env}.pem`);
+let buffer = null;
+try {
+    buffer = fs.readFileSync(`${__dirname}/config/privateKeys/${env}.pem`);
+} catch (e) {
+    console.warn('Unable to read the private key');
+    console.warn(e.message);
+}
 
-module.exports = JSON.stringify(buffer.toString('utf-8'));
+module.exports = JSON.stringify((buffer || '').toString('utf-8'));
